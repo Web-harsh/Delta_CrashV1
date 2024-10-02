50a56e8f8109bb04141f6a0689768ed7c2c9e78c
Web-harsh <godwinoloche50@gmail.com>
2024-10-02 03:27:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "6285381775839"
+let phoneNumber = "2348141104435"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
